6ae88a5b112227929bed2b65eca883e642714c68
Sascha <sascha.grotz@web.de>
2019-05-03 09:14:25 +0200

    Fix Strings

--- src/de/sgrotz/se/refactoring/Customer.java
+++ src/de/sgrotz/se/refactoring/Customer.java
@@ -22,7 +22,7 @@ public class Customer {
         double totalAmount = 0;
         int frequentRenterPoints = 0;
         Enumeration enum_rentals = rentals.elements();	    
-        String result = "de.sgrotz.se.refactoring.Rental Record for " + this.getName() + "\n";
+        String result = "Rental Record for " + this.getName() + "\n";
         result += "\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n";
 
         while (enum_rentals.hasMoreElements()) {
